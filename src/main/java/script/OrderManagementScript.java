c91c07028771201a2f5424e6ad9b703a454e6a2e
Donato Marrazzo <donato.marrazzo@gmail.com>
2019-05-06 17:47:19 +0200

    autoApprovalRulesEntry: setting order price

--- src/main/java/script/OrderManagementScript.java
+++ src/main/java/script/OrderManagementScript.java
@@ -23,12 +23,21 @@ public class OrderManagementScript {
     public static void prepareOfferExit(ProcessContext kcontext) {
         List<SupplierInfo> suppliersInfoList = (List<SupplierInfo>) kcontext.getVariable("suppliersInfoList");
         SupplierInfo supplierInfo = (SupplierInfo) kcontext.getVariable("supplierInfo");
+
         System.out.println("supplier offer: "+supplierInfo.getOffer());
 
         suppliersInfoList.add(supplierInfo);
         kcontext.setVariable("suppliersInfoList",suppliersInfoList);
         suppliersInfoList.stream().min(Comparator.comparing(SupplierInfo::getOffer)).ifPresent(s -> {
-            kcontext.setVariable("supplierInfo", s);           
+            kcontext.setVariable("supplierInfo", s);
         });
+    }
+
+    // Auto Approval Rules
+    public static void autoApprovalRulesEntry(ProcessContext kcontext) {
+        OrderInfo orderInfo = (OrderInfo) kcontext.getVariable("orderInfo");
+        SupplierInfo supplierInfo = (SupplierInfo) kcontext.getVariable("supplierInfo");
+        orderInfo.setPrice(supplierInfo.getOffer());
+        kcontext.setVariable("orderInfo", orderInfo);
     }
 }
